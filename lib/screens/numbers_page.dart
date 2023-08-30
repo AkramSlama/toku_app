d6ea07982b62e5987a46991d92b752de1a106803
akram <akramslamas8@gmail.com>
2023-08-30 14:57:09 +0300

    Refactor Numbers Page

--- lib/screens/numbers_page.dart
+++ lib/screens/numbers_page.dart
@@ -2,66 +2,70 @@ import 'package:flutter/material.dart';
 import 'package:toku_app/components/item.dart';
 import '../models/item_model.dart';
 
+// a5dt balak
+// lazem ba7aded el changes
+// b3ml refresh w a5tarhom kolhm
+
 class NumbersPage extends StatelessWidget {
   const NumbersPage({Key? key}) : super(key: key);
   final List<ItemModel> number = const [
     ItemModel(
       sound: 'sounds/numbers/number_one_sound.mp3',
-      jpName: 'Eins',
+      jpName: '1つ',
       enName: 'one',
       image: 'assets/images/numbers/number_one.png',
     ),
     ItemModel(
       sound: 'sounds/numbers/number_two_sound.mp3',
-      jpName: 'zwei',
+      jpName: '二',
       enName: 'Two',
       image: 'assets/images/numbers/number_two.png',
     ),
     ItemModel(
       sound: 'sounds/numbers/number_three_sound.mp3',
-      jpName: 'Drei',
+      jpName: '三つ',
       enName: 'three',
       image: 'assets/images/numbers/number_three.png',
     ),
     ItemModel(
       sound: 'sounds/numbers/number_four_sound.mp3',
-      jpName: 'vier',
+      jpName: '四',
       enName: 'four',
       image: 'assets/images/numbers/number_four.png',
     ),
     ItemModel(
       sound: 'sounds/numbers/number_five_sound.mp3',
-      jpName: 'Funf',
+      jpName: '五',
       enName: 'five',
       image: 'assets/images/numbers/number_five.png',
     ),
     ItemModel(
       sound: 'sounds/numbers/number_six_sound.mp3',
-      jpName: 'sechs',
+      jpName: '六',
       enName: 'six',
       image: 'assets/images/numbers/number_six.png',
     ),
     ItemModel(
       sound: 'sounds/numbers/number_seven_sound.mp3',
-      jpName: 'Sieben',
+      jpName: 'セブン',
       enName: 'seven',
       image: 'assets/images/numbers/number_seven.png',
     ),
     ItemModel(
       sound: 'sounds/numbers/number_eight_sound.mp3',
-      jpName: 'Wunsche',
+      jpName: '八',
       enName: 'eight',
       image: 'assets/images/numbers/number_eight.png',
     ),
     ItemModel(
       sound: 'sounds/numbers/number_nine_sound.mp3',
-      jpName: 'neun',
+      jpName: '九',
       enName: 'nine',
       image: 'assets/images/numbers/number_nine.png',
     ),
     ItemModel(
       sound: 'sounds/numbers/number_ten_sound.mp3',
-      jpName: 'zehn',
+      jpName: 'テン',
       enName: 'ten',
       image: 'assets/images/numbers/number_ten.png',
     ),
